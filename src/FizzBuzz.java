255095e770ef05baa04b1691608b7edb853361d7
kobayakawa <m4bkgxpdr@gmail.com>
2024-09-04 21:46:52 +0900

    課題提出

---
+++ src/FizzBuzz.java
@@ -0,0 +1,27 @@
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		//1から100までの数値をループで処理する
+		for (int i = 1; i <= 100; i++) {
+			//3で割り切れる数値の場合、「Fizz」と表示する
+			if (i % 3 == 0 && i % 5 == 0) {
+				System.out.println("FizzBuzz");
+
+				//5で割り切れる数値の場合、「Buzz」と表示する
+			} else if (i % 5 == 0) {
+				System.out.println("Buzz");
+
+				//3と5の両方で割り切れる数値の場合、「FizzBuzz」と表示する
+			} else if (i % 3 == 0) {
+				System.out.println("Fizz");
+
+				//それ以外の数値は、その数値をそのまま表示する
+			} else {
+				System.out.println(i);
+			}
+		}
+	}
+}
